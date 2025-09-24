a4a9117517ca59b3ca65231138804b3d211e5137
armcool007 <bipinkk004@gmail.com>
2025-09-24 17:53:41 +0530

    Delete contact_backend_app_testing directory

--- contact_backend_app_testing/src/main/java/in/ashokit/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package in.ashokit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ashokit.binding.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Integer>{
-
-}
